48605202aaa2d2adc9fca8ad4d49d3847a707d5a
Hossein <hbehboudi@ce.sharif.edu>
2018-12-06 14:31:23 +0330

    News

--- web-project/src/App.js
+++ web-project/src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-
+import News from './component/news/News'
+import {Grid} from "semantic-ui-react";
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={6}>
+                            <News/>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ web-project/src/component/news/News.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import {Container, Divider, Dropdown, Grid, Menu} from "semantic-ui-react";
+import './News.css'
+
+class App extends Component {
+    state = {};
+    stateOptions = [{key: 'FB', value: 'FB', text: 'Football'}, {key: 'BB', value: 'BB', text: 'Basketball'}];
+
+    handleItemClick = (e, {name}) => this.setState({activeItem: name});
+
+    render() {
+        const {activeItem} = this.state;
+        return (
+            <div id="App">
+                <Menu fluid widths={2} inverted>
+                    <Menu.Item name='buy' active={activeItem === 'buy'} onClick={this.handleItemClick}/>
+                    <Menu.Item name='sell' active={activeItem === 'sell'} onClick={this.handleItemClick}/>
+                </Menu>
+
+                <Container textAlign='center'>
+                    <Dropdown placeholder='State' search selection options={this.stateOptions} inverted/>
+                </Container>
+
+                <Divider hidden/>
+                <Divider horizontal>
+                    News
+                </Divider>
+                <Container fluid textAlign='justified'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad exercitationem ipsam neque
+                    perferendis perspiciatis possimus sapiente sed, sit ut vitae. Animi atque, dolorem ea
+                    explicabo fuga ipsam molestiae non saepe.Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Ad exercitationem ipsam neque perferendis perspiciatis possimus
+                    sapiente sed, sit ut vitae. Animi atque, dolorem ea explicabo fuga ipsam molestiae non
+                    saepe.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad exercitationem ipsam
+                    neque perferendis perspiciatis possimus sapiente sed, sit ut vitae. Animi atque, dolorem
+                    ea explicabo fuga ipsam molestiae non saepe.Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Ad exercitationem ipsam neque perferendis perspiciatis possimus
+                    sapiente sed, sit ut vitae. Animi atque, dolorem ea explicabo fuga ipsam molestiae non
+                    saepe.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad exercitationem ipsam
+                    neque perferendis perspiciatis possimus sapiente sed, sit ut vitae. Animi atque, dolorem
+                    ea explicabo fuga ipsam molestiae non saepe.Lorem ipsum dolor sit amet, consectetur
+                    adipisicing elit. Ad exercitationem ipsam neque perferendis perspiciatis possimus
+                    sapiente sed, sit ut vitae. Animi atque, dolorem ea explicabo fuga ipsam molestiae non
+                    saepe.
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default App;
