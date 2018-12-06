834c0c8a334dcda134c82be1d1197f3f6188b6ed
Hossein <hbehboudi@ce.sharif.edu>
2018-12-06 19:03:39 +0330

    Slider created.

--- web-project/src/App.js
+++ web-project/src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
-import News from './component/news/News'
-import {Grid} from "semantic-ui-react";
+import News from './component/news/News';
+import Slider from './component/slider/Slider';
+import {Grid, Image} from "semantic-ui-react";
+import {MenuBar} from "./component/menu-bar/MenuBar";
+
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Grid>
                     <Grid.Row>
-                        <Grid.Column width={6}>
-                            <News/>
+                        <Grid.Column width={16}>
+                            <MenuBar/>
                         </Grid.Column>
+                    </Grid.Row>
+
+                    <Grid.Row>
+                    <Grid.Column width={6}>
+                    <News/>
+                    </Grid.Column>
+                    <Grid.Column width={10}>
+                        <Slider/>
+                    </Grid.Column>
                     </Grid.Row>
                 </Grid>
             </div>
