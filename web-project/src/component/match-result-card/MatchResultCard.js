2773f5b720ff860ea404ccb471b6188f9103e65a
aafsari <amirafsari99@yahoo.com>
2018-12-09 20:49:36 +0330

    small changes.

--- web-project/src/component/match-result-card/MatchResultCard.js
+++ web-project/src/component/match-result-card/MatchResultCard.js
@@ -6,15 +6,13 @@ import DirectionProvider, {DIRECTIONS} from 'react-with-direction/dist/Direction
 import {Image} from "../util/image";
 
 
-
-
 export class MatchResultCard extends React.Component {
     render() {
         return (
             <DirectionProvider direction={DIRECTIONS.LTR}>
                 <Card className={'match-result'} id="card">
                     <Card.Content>
-                        <Card.Header>UEFA Champions League</Card.Header>
+                        <Card.Header className={'league'}>UEFA Champions League</Card.Header>
                     </Card.Content>
                     <Card.Content>
                         <Feed>
@@ -34,7 +32,7 @@ export class MatchResultCard extends React.Component {
                             <Feed.Event>
                                 <Feed.Label image={<Image
                                     src="http://www.stickpng.com/assets/images/584a9b47b080d7616d298778.png"
-                                    alt="could not load" width={40} height={40} mode={'fit'} />}/>
+                                    alt="could not load" width={40} height={40} mode={'fit'}/>}/>
                                 <Feed.Content>
                                     <Feed.Date content='Real Madrid'/>
                                     <Feed.Summary className={'goals-text'}>
--- web-project/src/component/util/slideshow/SlideShow.js
+++ web-project/src/component/util/slideshow/SlideShow.js
@@ -13,7 +13,7 @@ export class SimpleSlider extends React.Component {
             dots: true,
             infinite: true,
             speed: 500,
-            slidesToShow: 5,
+            slidesToShow: 2,
             slidesToScroll: 1,
             autoplay: true,
             autoplaySpeed: 3000,
