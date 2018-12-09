3a4fd8e18c704a70b5bc9acbe865aa381bae37c6
aafsari <amirafsari99@yahoo.com>
2018-12-09 23:50:37 +0330

    responsive menu bar.

--- web-project/src/component/menu-bar/MenuBar.js
+++ web-project/src/component/menu-bar/MenuBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './MenuBar.css';
 import _ from 'lodash'
-import {Menu, Segment, Search, Grid, Icon, Dropdown, Transition} from 'semantic-ui-react'
+import {Menu, Segment, Grid, Icon, Dropdown, Transition, Search, Responsive} from 'semantic-ui-react'
 import DirectionProvider, {DIRECTIONS} from 'react-with-direction/dist/DirectionProvider';
 
 
@@ -9,38 +9,110 @@ export class MenuBar extends React.Component {
     state = {
         activeItem: 'home',
 
-    }
+    };
 
-    handleItemClick = (e, {name}) => this.setState({activeItem: name})
+    handleItemClick = (e, {name}) => this.setState({activeItem: name});
 
     render() {
         const {activeItem} = this.state;
 
         return (
             <DirectionProvider direction={DIRECTIONS.RTL}>
-                <Segment inverted  >
-                    <Menu inverted pointing secondary className={'menu-bar'}>
-                        <Menu.Item name="Sport" id={'site-name'}/>
-                        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
-                        <Menu.Item
-                            name='messages'
-                            active={activeItem === 'messages'}
-                            onClick={this.handleItemClick}
-                        />
-                        <Menu.Item
-                            name='friends'
-                            active={activeItem === 'friends'}
-                            onClick={this.handleItemClick}
-                        />
-                        <Menu.Item id={'search-bar'} position='left'>
-                            <SearchExampleStandard/>
-                        </Menu.Item>
-                        <Menu.Item id='profile-icon' /*position='left'*/>
-                            <ProfileIcon/>
-                        </Menu.Item>
 
-                    </Menu>
+                <Segment inverted>
+                    <Responsive minWidth={768}>
+
+                        <Menu inverted pointing secondary className={'menu-bar'}>
+                            <Menu.Item name="Sport" id={'site-name'}/>
+                            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
+                            <Menu.Item
+                                name='messages'
+                                active={activeItem === 'messages'}
+                                onClick={this.handleItemClick}
+                            />
+                            <Menu.Item
+                                name='friends'
+                                active={activeItem === 'friends'}
+                                onClick={this.handleItemClick}
+                            />
+                            <Menu.Item id={'search-bar'} position='left'>
+
+                                <SearchExampleStandard/>
+                            </Menu.Item>
+
+                            <Menu.Item id='profile-icon' >
+                                <ProfileIcon/>
+                            </Menu.Item>
+
+                            <Menu.Item id='search-icon'>
+
+
+                            </Menu.Item>
+
+                        </Menu>
+                    </Responsive>
+                    <Responsive maxWidth={768} minWidth={540}>
+
+                        <Menu inverted pointing secondary className={'menu-bar'}>
+                            <Menu.Item name="Sport" id={'site-name'}/>
+                            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
+                            <Menu.Item
+                                name='messages'
+                                active={activeItem === 'messages'}
+                                onClick={this.handleItemClick}
+                            />
+                            <Menu.Item
+                                name='friends'
+                                active={activeItem === 'friends'}
+                                onClick={this.handleItemClick}
+                            />
+                            <Menu.Item id={'search-icon'} position='left'>
+                                <SearchIcon/>
+                            </Menu.Item>
+
+                            <Menu.Item id='profile-icon'>
+                                <ProfileIcon/>
+                            </Menu.Item>
+
+                            <Menu.Item id='search-icon'>
+
+
+                            </Menu.Item>
+
+                        </Menu>
+                    </Responsive>
+                    <Responsive maxWidth={540}>
+
+                        <Menu inverted pointing secondary className={'menu-bar'}>
+                            <Menu.Item
+                                icon='home'
+                                active={activeItem === 'home'}
+                                onClick={this.handleItemClick}/>
+                            <Menu.Item
+                                icon='trophy'
+                                active={activeItem === 'messages'}
+                                onClick={this.handleItemClick}
+                            />
+                            <Menu.Item
+                                icon='video'
+                                active={activeItem === 'friends'}
+                                onClick={this.handleItemClick}
+                            />
+                            <Menu.Item id='search-icon' position='left'>
+                                <SearchIcon/>
+                            </Menu.Item>
+
+
+                            <Menu.Item id='profile-icon'>
+                                <ProfileIcon/>
+                            </Menu.Item>
+
+
+                        </Menu>
+                    </Responsive>
+
                 </Segment>
+
             </DirectionProvider>
         )
     }
@@ -96,7 +168,7 @@ export class ProfileIcon extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            color: 'grey',
+            color: 'black',
             dropdown: false,
         }
     }
@@ -109,16 +181,15 @@ export class ProfileIcon extends React.Component {
 
     mouseLeave = () => {
         if (!this.state.dropdown)
-            this.setState({color: 'grey'})
+            this.setState({color: 'black'})
     };
 
     dropdown = () => {
         this.setState({dropdown: !this.state.dropdown});
         if (this.state.dropdown)
-            this.setState({color: 'grey'});
+            this.setState({color: 'black'});
         else
             this.setState({color: 'teal'})
-
 
 
     };
@@ -128,7 +199,7 @@ export class ProfileIcon extends React.Component {
         const {dropdown} = this.state;
         return (
             <DirectionProvider direction={DIRECTIONS.RTL}>
-                <Dropdown inverted icon={<Icon circular inverted size='large' name='user' color={this.state.color}
+                <Dropdown inverted icon={<Icon circular inverted name='user' color={this.state.color}
                                                onClick={this.dropdown} onMouseEnter={this.mouseEnter}
                                                onMouseLeave={this.mouseLeave}/>}>
                     <Transition visible={dropdown} animation='slide down' duration={250}>
@@ -145,15 +216,53 @@ export class ProfileIcon extends React.Component {
 
 }
 
-
-export class TransitionTest extends React.Component {
-
-    /*
-
-        render() {
-            return (   );
+export class SearchIcon extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: 'black',
+            dropdown: false,
         }
-    */
+    }
+
+    mouseEnter = () => {
+        if (!this.state.dropdown)
+            this.setState({color: 'teal'})
+
+    };
+
+    mouseLeave = () => {
+        if (!this.state.dropdown)
+            this.setState({color: 'black'})
+    };
+
+    dropdown = () => {
+        this.setState({dropdown: !this.state.dropdown});
+        if (this.state.dropdown)
+            this.setState({color: 'black'});
+        else
+            this.setState({color: 'teal'})
+
+
+    };
+
+
+    render() {
+        const {dropdown} = this.state;
+        return (
+            <DirectionProvider direction={DIRECTIONS.RTL}>
+                <Dropdown inverted icon={<Icon circular inverted name='search' color={this.state.color}
+                                               onClick={this.dropdown} onMouseEnter={this.mouseEnter}
+                                               onMouseLeave={this.mouseLeave}/>}>
+                    <Transition visible={dropdown} animation='slide down' duration={250}>
+                        <Dropdown.Menu inverted className={'search-dropdown'}>
+                            <SearchExampleStandard/>
+                        </Dropdown.Menu>
+                    </Transition>
+                </Dropdown>
+            </DirectionProvider>)
+    }
 
 }
+
 
