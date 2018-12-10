b82911fef3d6334a476e8fc177e17e468a12256c
aafsari <amirafsari99@yahoo.com>
2018-12-10 12:38:48 +0330

    league-dashbard

--- web-project/src/component/leagues-dashboard/LeagueDashboard.js
+++ web-project/src/component/leagues-dashboard/LeagueDashboard.js
@@ -3,7 +3,7 @@ import { Segment, Grid, Divider} from "semantic-ui-react";
 import './LeagueDashboard.css'
 import DirectionProvider, {DIRECTIONS} from "react-with-direction/dist/DirectionProvider";
 import LeagueTable from "../league-table";
-import {MatchResultCard} from "../match-result-card";
+import {SmallMatchCard} from "../small-match-card";
 
 
 export class LeagueDashboard extends React.Component {
@@ -16,8 +16,13 @@ export class LeagueDashboard extends React.Component {
             <DirectionProvider direction={DIRECTIONS.RTL}>
                 <Segment>
                     <Grid columns={2} relaxed='very'>
-                        <Grid.Column>
-                            <MatchResultCard/>
+                        <Grid.Column className={'match-results'}>
+
+                            <SmallMatchCard/>
+                            <SmallMatchCard/>
+                            <SmallMatchCard/>
+                            <SmallMatchCard/>
+
                         </Grid.Column>
                         <Grid.Column>
                             <LeagueTable/>
---
+++ web-project/src/component/small-match-card/SmallMatchCard.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Icon, Feed} from "semantic-ui-react";
+import './SmallMatchCard.css'
+import DirectionProvider, {DIRECTIONS} from "react-with-direction/dist/DirectionProvider";
+
+
+export class SmallMatchCard extends React.Component {
+
+    render() {
+
+
+        return (
+
+            <DirectionProvider direction={DIRECTIONS.RTL}>
+                <Feed className={'results'}>
+                    <Feed.Event >
+                        <Feed.Label className={'home-logo'}>
+                            <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+                        </Feed.Label>
+                        <Feed.Content className={'goals'}>
+                            <div className={'home-goals'}>2</div>
+                        </Feed.Content>
+                        <div className={'time'}>
+                            <Icon name='red clock'/>
+                            <div>49</div>
+                        </div>
+                        <Feed.Content className={'goals'}>
+                            <div className={'visitor-goals'}>0</div>
+                        </Feed.Content>
+                        <Feed.Label className={'visitor-logo'}>
+                            <img src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+                        </Feed.Label>
+                    </Feed.Event>
+                </Feed>
+            </DirectionProvider>
+        )
+    }
+}
