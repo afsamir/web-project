30f8ee8ad58ccb38aefdd2eeb29a9ae4f1ea070d
aafsari <amirafsari99@yahoo.com>
2018-12-11 02:10:12 +0330

    Home page responsive.

--- web-project/src/component/news-div/NewsDiv.js
+++ web-project/src/component/news-div/NewsDiv.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './NewsDiv.css';
 
 import DirectionProvider, {DIRECTIONS} from 'react-with-direction/dist/DirectionProvider';
-import {Feed, Icon, Image, Label} from "semantic-ui-react";
+import {Icon, Image, Responsive} from "semantic-ui-react";
 
 
 export class NewsDiv extends React.Component {
@@ -15,7 +15,9 @@ export class NewsDiv extends React.Component {
     render() {
         return (
             <DirectionProvider direction={DIRECTIONS.RTL} >
+
                 <p className={'news-div'}>
+                    <Responsive minWidth={768}>
                     <Icon className={"icon"}/>
 
                     <p className={'news-text'}>
@@ -25,8 +27,21 @@ export class NewsDiv extends React.Component {
                         <div className={'news-date'}>3 days ago</div>
 
                     </p>
+                    </Responsive>
+                    <Responsive maxWidth={767} >
+                        <Icon className={"icon"}/>
 
+                        <p className={'news-text'}>
+                            <Icon name={'angle left'} className={'main'}/>
+                            <Image size={'tiny'} src={"https://react.semantic-ui.com/images/wireframe/image.png"}
+                                   className={'news-image'}/> یورگن کلوپ:قهرمانی بارسلونا من را شگفت زده کرد!
+                            <div className={'news-date'}>3 days ago</div>
+
+                        </p>
+                    </Responsive>
                 </p>
+
+
             </DirectionProvider>
         )
     }
