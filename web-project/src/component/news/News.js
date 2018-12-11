be6fa0066383c00ab7a0997dad2c905f7ab97863
Hossein <hbehboudi@ce.sharif.edu>
2018-12-11 12:03:11 +0330

    Updated.:))

--- web-project/src/component/news/News.js
+++ web-project/src/component/news/News.js
@@ -27,7 +27,7 @@ class App extends Component {
                 <Divider hidden/>
                 <Divider horizontal>News</Divider>
 
-                <Segment fluid textAlign='justified' style={{overflow: 'auto', maxHeight: 1200 }}>
+                <Segment fluid textAlign='justified' style={{overflow: 'auto', height: '1314px' }}>
                     <NewsDiv />
                     <NewsDiv/>
                     <NewsDiv/>
